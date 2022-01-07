0e6c65a2626bfd2a29ebbe8e3f5c566314a290dc
Abel <abel.tarazona@urp.edu.pe>
2022-01-07 15:23:28 -0500

    Add space

--- app/src/main/java/com/abeltarazona/challenge/ui/MainActivity.kt
+++ app/src/main/java/com/abeltarazona/challenge/ui/MainActivity.kt
@@ -27,6 +27,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
